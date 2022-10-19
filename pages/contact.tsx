047303c555b94d6a2f947ae23010e2c3a5f19882
Erik Karwatowski <erik@macbook-air.mynetworksettings.com>
2022-10-19 18:58:20 -0400

    fix contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,8 +1,42 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Router from 'next/router';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import styles from '../styles/Contact.module.css'
 
+interface Inputs {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const schema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  message: yup.string().required(),
+}).required();
+
 const Contact: NextPage = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = (data: Inputs) => {
+    fetch('https://formspree.io/f/myyvddbl', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+          'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        Router.push('thanks');
+      }
+    })
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,27 +45,26 @@ const Contact: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <form className={styles.form} action="https://formspree.io/f/yeserikcan@gmail.com" method="POST">
+      <form id="contact-form" className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.group}>
-          <input className={styles.input} type="text" name="name" required />
+          <input className={styles.input} type="text" {...register("name")} required />
           <label className={styles.label} htmlFor="name">Name</label>
           <div className={styles.bar}></div>
         </div>
 
         <div className={styles.group}>
-          <input className={styles.input} type="email" name="_replyto" required />
-          <label className={styles.label} htmlFor="_replyto">Email</label>
+          <input className={styles.input} type="email" {...register("email")} required />
+          <label className={styles.label} htmlFor="email">Email</label>
           <div className={styles.bar}></div>
         </div>
 
         <div className={styles.group}>
-          <input className={styles.input} type="text" name="message" required />
+          <input className={styles.input} type="text" {...register("message")}required />
           <label className={styles.label} htmlFor="message">Message</label>
           <div className={styles.bar}></div>
         </div>
 
-        <input className={styles.input} type="hidden" name="_next" value="/thanks" />
-        <input className={styles.input} type="submit" value="Submit" />
+        <button className={styles.button}>Submit</button>
       </form>
 
     </div>
