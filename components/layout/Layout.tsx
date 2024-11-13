fdccebb8fe6fe079480836ba4f6195a954a289b0
Erik Karwatowski <ekarwatowski@gmail.com>
2024-11-13 14:46:41 -0500

    console to anyone inspecting site

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -3,6 +3,7 @@ import Footer from '../footer/Footer'
 import styles from '../../styles/Layout.module.css'
 
 export default function Layout({ children }: Record<string, React.ReactNode>) {
+  console.log('Hey there! If you\'re inspecting this page, you should hire me! I\'m a great teammate and I\'m looking for a job right now!'); 
   return (
     <div className={styles.layout}>
       <Navbar />
