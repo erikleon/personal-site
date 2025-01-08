b59ccf8eb3b2c8fbd1f5c6e57e5f18d8a77a67c9
Erik Karwatowski <ekarwatowski@gmail.com>
2025-01-08 14:04:42 -0500

    swap font display instead of optional

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
- 
+import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arvo|Montserrat&display=optional" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Arvo|Montserrat&display=swap"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
