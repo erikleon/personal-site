532c3fe6cf243bf21d79bb95d647a128a67541d3
Erik Karwatowski <ekarwatowski@gmail.com>
2025-01-08 14:04:56 -0500

    remove tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Script from 'next/script';
-import Head from 'next/head';
-import Layout from '../components/layout/Layout';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script  async src="https://www.googletagmanager.com/gtag/js?id=UA-36347110-5"></Script>
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-36347110-5');
-        `}
-      </Script>
       <Head>
-        
-
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
@@ -31,4 +17,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp
+export default MyApp;
