77056d01ad73f23002534dc1bf92d0b35c48a799
Erik Karwatowski <ekarwatowski@gmail.com>
2025-07-29 22:14:45 -0400

    upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require('./scripts/generate-sitemap')
+      require("./scripts/generate-sitemap");
     }
 
-    return config
+    return config;
   },
-}
+};
