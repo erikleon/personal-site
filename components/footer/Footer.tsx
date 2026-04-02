2242d17745a677022d0d36d46f4dbe194bf4676c
Erik Karwatowski <ekarwatowski@gmail.com>
2026-04-02 17:03:05 -0400

    feat(footer): add session-gated Tools link

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,16 +1,45 @@
-import Link from "next/link"
-import styles from '../../styles/Navigation.module.css'
+import Link from "next/link";
+import styles from "../../styles/Navigation.module.css";
+import { useSession } from "next-auth/react";
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <Link href="/Karwatowski_Erik_Resume.pdf" target="_blank" rel="noreferrer" className={styles.pageLink}>
-      Resume
-    </Link>
+const Footer = () => {
+  const { data: session } = useSession();
 
-    <a className={styles.pageLink} target="_blank" rel="noreferrer" href="https://github.com/erikleon/">GitHub</a>
+  return (
+    <footer className={styles.footer}>
+      {session && (
+        <Link href="/tools" className={styles.pageLink}>
+          Tools
+        </Link>
+      )}
+      <Link
+        href="/Karwatowski_Erik_Resume.pdf"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.pageLink}
+      >
+        Resume
+      </Link>
 
-    <a className={styles.pageLink} target="_blank" rel="noreferrer" href="https://linkedin.com/in/erikkarwatowski/">LinkedIn</a>
-  </footer>
-)
+      <a
+        className={styles.pageLink}
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/erikleon/"
+      >
+        GitHub
+      </a>
 
-export default Footer
+      <a
+        className={styles.pageLink}
+        target="_blank"
+        rel="noreferrer"
+        href="https://linkedin.com/in/erikkarwatowski/"
+      >
+        LinkedIn
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
